0202aafe8eca5db0c2700ef99146239d283820a7
Raza <141234596+KMORaza@users.noreply.github.com>
2023-08-05 19:50:26 +0530

    Delete ZTransform.m

--- MATLAB Codes/ZTransform.m
+++
@@ -1,8 +0,0 @@
-clc
-clear all
-syms z Y n positive
-LHS=ztrans(sym('y(n+2)')-sym('y(n+1)')-sym('y(n)'),n,z);
-RHS=ztrans(0,n,z);
-newLHS=subs(LHS,{'ztrans(y(n),n,z)','y(0)','y(1)'},{Y,0,1});
-Y=solve(newLHS-RHS,Y);
-y=iztrans(Y,z,n);
